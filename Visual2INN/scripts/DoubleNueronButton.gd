8e315e2ca77ea92d5ebdde19be940f38b9fb3cc3
vivisuke <ntsuda@master.email.ne.jp>
2023-07-25 08:53:39 +0900

    0100 ◎ C 07/25 07/25 0.002│ToDo│トップシーン：各パネル押下時は影を小さく

--- Visual2INN/scripts/DoubleNueronButton.gd
+++ Visual2INN/scripts/DoubleNueronButton.gd
@@ -101,7 +101,7 @@ func _draw():
 	var style_box = StyleBoxFlat.new()      # 影、ボーダなどを描画するための矩形スタイルオブジェクト
 	style_box.bg_color = Color.WHITE if !selected else Color.LIGHT_CYAN  # 矩形背景色
 	style_box.shadow_offset = Vector2(0, 4)     # 影オフセット
-	style_box.shadow_size = 8                   # 影（ぼかし）サイズ
+	style_box.shadow_size = 8 if !selected else 1		# 影（ぼかし）サイズ
 	style_box.shadow_color = Color.GRAY
 	draw_style_box(style_box, Rect2(Vector2(0, 0), self.size))      # style_box に設定した矩形を描画
 	# エッジ
